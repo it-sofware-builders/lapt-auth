2bce8728859e9a231df1b226ceb81a89c1d7080f
Lucas Kouami <rkouami@outlook.com>
2022-07-10 20:56:30 +0000

    add get all users

--- lapt-auth/Auth/Response.cs
+++ lapt-auth/Auth/Response.cs
@@ -4,4 +4,7 @@ public class Response
 {
     public string? Status { get; set; }
     public string? Message { get; set; }
+
+    // add data to the response
+    public object? Data { get; set; }
 }
---
+++ lapt-auth/Auth/User.cs
@@ -0,0 +1,7 @@
+﻿namespace lapt_auth.Auth;
+
+public class User
+{
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+}
